3cd10efeadf438a06dee7e8398a6c619c33fd71b
Schoomie <jakubpatecki97@gmail.com>
2019-02-05 00:08:24 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>TODO</title>
+
+
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="css/main.css">
+    <meta name="viewport" content="width=device-width,  initial-scale=1.0">
+</head>
+
+<body>
+
+<div class="list">
+    <h1 class="header"> Todo.</h1>
+
+    <ul>
+        <li><span class="item ">Pick up shopping</span></li>
+        <li><span class="item done">Learn php</span></li>
+    </ul>
+
+    <form class="item-add" action="add.php" method="post" >
+
+
+    </form>
+
+
+
+</div>
+
+
+</body>
+
+</html>
