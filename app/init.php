fe1e481e8a1d72b1c3b82c27475c03aaf6ea63c8
Schoomie <jakubpatecki97@gmail.com>
2019-02-07 17:32:05 +0100

    gogo

--- app/init.php
+++ app/init.php
@@ -2,9 +2,25 @@
 
 session_start();
 
-$SESSION['user_id'] = 1;
+$_SESSION['user_id'] = 1;
 
- $db = new PDO('mysql:dbname=todo;host=localhost','root','root');
+
+try {
+//    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+
+    $db = new PDO('mysql:dbname=awidlak_abcd;host=88.198.33.199','awidlak_abcd','Abcdef1234!');
+
+    // set the PDO error mode to exception
+//    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e)
+{
+    die("Connection failed: " . $e->getMessage());
+}
+
+if(!isset($_SESSION['user_id'])) {
+    die('You are not signed in');
+}
 
 
 
--- index.php
+++ index.php
@@ -1,22 +1,25 @@
 <?php
 
 require_once 'app/init.php';
+
 $itemsQuery = $db->prepare("
         SELECT id,name,done
         FROM items
-        WHERE user =: user 
+        WHERE user = :user
         "
 );
-$itemsQuery->execute([
-        'user' => $_SESSION['user_id']
-]);
-
-$items = $itemsQuery->rowCount() ? $itemsQuery : [];
-foreach($items as $item){
-    echo $item['name'], '<br>';
-}
+$itemsQuery->bindParam(':user', $_SESSION['user_id'], PDO::PARAM_INT);
 
 
+$itemsQuery->execute();
+
+$items = $itemsQuery->fetchAll(PDO::FETCH_ASSOC);
+
+
+// var_dump();
+// die();
+// php -S localhost:8004
+// haslo: Abcdef1234!
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -37,16 +40,20 @@ foreach($items as $item){
 <div class="list">
     <h1 class="header"> Todo.</h1>
 
+    <?php if(!empty($items)): ?>
     <ul class="items">
+        <?php foreach($items as $item): ?>
         <li>
-            <span class="item ">Pick up shopping</span>
+            <span class="item<?php echo$item['done'] ? 'done' : '' ?>"><?php echo $item['name']; ?></span>
+            <?php if(!$item['done']): ?>
             <a href="#" class="done-button">Mark as done</a>
+            <?php endif; ?>
         </li>
-        <li>
-            <span class="item done">Learn php</span>
-        </li>
+        <?php endforeach; ?>
     </ul>
-
+<?php else: ?>
+        <p>You haven't added any items yet. </p>
+    <?php endif; ?>
     <form class="item-add" action="add.php" method="post" >
         <input type="text" name="name" placeholder="Type a new item here." class="input" autocomplete="off" required>
         <input type="submit" value="Add" class="submit">
