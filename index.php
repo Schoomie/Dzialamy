4ca60a75b5e790a4d05c651802b5f30869353528
Schoomie <jakubpatecki97@gmail.com>
2019-02-08 00:42:29 +0100

    gogo

--- index.php
+++ index.php
@@ -11,15 +11,15 @@ $itemsQuery = $db->prepare("
 
 
 $itemsQuery->execute(
-        [
-                'user'=> $_SESSION['user_id']
-        ]
+    [
+        'user' => $_SESSION['user_id']
+    ]
 );
 
-$items = $itemsQuery->rowCount () ? $itemsQuery : [];
+$items = $itemsQuery->rowCount() ? $itemsQuery : [];
 
 //foreach($items as $item) {
-   // print_r($item);
+// print_r($item);
 
 
 // var_dump();
@@ -46,26 +46,25 @@ $items = $itemsQuery->rowCount () ? $itemsQuery : [];
 <div class="list">
     <h1 class="header"> Todo.</h1>
 
-    <?php if(!empty($items)): ?>
-    <ul class="items">
-        <?php foreach($items as $item): ?>
-        <li>
-            <span class="item<?php echo $item['done'] ? 'done' : '' ?>"><?php echo $item['name']; ?></span>
-            <?php if(!$item['done']): ?>
-            <a href="mark.php?as=done&item<?php echo $item['id']?>" class="done-button">Mark as done</a>
-            <?php endif; ?>
-        </li>
-        <?php endforeach; ?>
-    </ul>
-<?php else: ?>
+    <?php if (!empty($items)): ?>
+        <ul class="items">
+            <?php foreach ($items as $item): ?>
+                <li>
+                    <span class="item<?php echo $item['done'] ? 'done' : '' ?>"><?php echo $item['name']; ?></span>
+                    <?php if (!$item['done']): ?>
+                        <a href="mark.php?as=done&item<?php echo $item['id'] ?>" class="done-button">Mark as done</a>
+                    <?php endif; ?>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    <?php else: ?>
         <p>You haven't added any items yet. </p>
     <?php endif; ?>
-    <form class="item-add" action="add.php" method="post" >
+    <form class="item-add" action="add.php" method="post">
         <input type="text" name="name" placeholder="Type a new item here." class="input" autocomplete="off" required>
         <input type="submit" value="Add" class="submit">
 
     </form>
-
 
 
 </div>
