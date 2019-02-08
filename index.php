3911d5fdd4bacaa3cc022c411e64a0a1eb6c3b7a
Schoomie <jakubpatecki97@gmail.com>
2019-02-08 01:46:54 +0100

    zabawa

--- index.php
+++ index.php
@@ -1,75 +1,48 @@
 <?php
-
 require_once 'app/init.php';
-
-$itemsQuery = $db->prepare("
-        SELECT id,name,done
-        FROM items
-        WHERE user = :user
-        "
-);
-
-
-$itemsQuery->execute(
-    [
-        'user' => $_SESSION['user_id']
-    ]
-);
-
+$itemsQuery = $db->prepare("SELECT id, name, done FROM items WHERE user = :user");
+$itemsQuery->execute([
+    'user' => $_SESSION['user_id']
+]);
 $items = $itemsQuery->rowCount() ? $itemsQuery : [];
-
-//foreach($items as $item) {
-// print_r($item);
-
-
-// var_dump();
-// die();
-// php -S localhost:8004
-// haslo: Abcdef1234!
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>TODO</title>
-
-
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-
-    <link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel="stylesheet">
+    <title>To do</title>
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans">
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Shadows+Into+Light+Two">
     <link rel="stylesheet" href="css/main.css">
-    <meta name="viewport" content="width=device-width,  initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
-
 <body>
-
 <div class="list">
-    <h1 class="header"> Todo.</h1>
+    <h1 class="header">To do.</h1>
 
-    <?php if (!empty($items)): ?>
+    <?php if(!empty($items)): ?>
         <ul class="items">
-            <?php foreach ($items as $item): ?>
+            <?php foreach($items as $item): ?>
                 <li>
-                    <span class="item<?php echo $item['done'] ? 'done' : '' ?>"><?php echo $item['name']; ?></span>
-                    <?php if (!$item['done']): ?>
-                        <a href="mark.php?as=done&item<?php echo $item['id'] ?>" class="done-button">Mark as done</a>
+                    <span class="item<?php echo $item['done'] ? ' done' : ''?>"> <?php echo ($item['name']); ?></span>
+                    <a class="delete-button" href="mark.php?as=delete&item=<?php echo $item['id']; ?>">Delete</a>
+                    <?php if(!$item['done']): ?>
+                        <a class="done-button" href="mark.php?as=done&item=<?php echo $item['id']; ?>">Mark as done</a>
+                    <?php else: ?>
+                        <a class="undone-button" href="mark.php?as=undone&item=<?php echo $item['id']; ?>">Mark as undone</a>
                     <?php endif; ?>
                 </li>
             <?php endforeach; ?>
         </ul>
     <?php else: ?>
-        <p>You haven't added any items yet. </p>
+        <p>You haven't added any items yet.</p>
     <?php endif; ?>
+
     <form class="item-add" action="add.php" method="post">
         <input type="text" name="name" placeholder="Type a new item here." class="input" autocomplete="off" required>
         <input type="submit" value="Add" class="submit">
-
     </form>
-
-
 </div>
 
-
 </body>
-
 </html>
--- mark.php
+++ mark.php
@@ -1,29 +1,45 @@
 <?php
-
 require_once 'app/init.php';
-
-if (isset($GET['as'], $_GET['item'])) {
-
+if(isset($_GET['as'], $_GET['item'])){
     $as = $_GET['as'];
     $item = $_GET['item'];
-
-    switch ($as) {
-        case'done':
+    switch($as){
+        case 'done':
             $doneQuery = $db->prepare("
-        UPDATE items
-        SET done = 1
-        WHERE id= :item
-        AND user =:user
-        
- 
- 
-        ");
+					UPDATE items SET done = 1
+					WHERE id = :item
+					AND user = :user
+					");
             $doneQuery->execute([
+                'item' => $item,
+                'user' => $_SESSION['user_id']
+            ]);
+            break;
+
+        case 'undone':
+            $undoneQuery = $db->prepare("
+				UPDATE items
+				SET done = 0
+				WHERE id = :item
+				AND user = :user
+			");
+            $undoneQuery->execute([
+                'item' => $item,
+                'user' => $_SESSION['user_id']
+            ]);
+            break;
+        case 'delete':
+            $deleteQuery = $db->prepare("
+				DELETE FROM items
+				WHERE id = :item
+				AND user = :user
+			");
+            $deleteQuery->execute([
                 'item' => $item,
                 'user' => $_SESSION['user_id']
             ]);
             break;
     }
 }
-
-header('Location:index.php');
+header('Location: index.php')
+?>
